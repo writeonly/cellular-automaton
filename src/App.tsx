cdde875d09d4a2ad91a9bcdbf19323ebc0d0b93f
Kamil Adam <kamil.adam.zabinski@gmail.com>
2024-11-03 20:06:49 +0100

    Add routering

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import AntApp from './ant/components/AntApp';
 import GameOfLifeApp from './gameOfLife/components/GameOfLifeApp';
 
 const App: React.FC = () => {
-//       <AntApp />
   return (
-    <div className="App">
-      <GameOfLifeApp />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/ant">Langton's Ant</Link>
+            </li>
+            <li>
+              <Link to="/game-of-life">Game of Life</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/ant" element={<AntApp />} />
+          <Route path="/game-of-life" element={<GameOfLifeApp />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
